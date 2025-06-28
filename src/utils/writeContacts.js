f88ab084340210f35a68ca58ede1eebf1cf69828
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-06-28 23:55:10 +0300

    fix

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -3,11 +3,7 @@ import { PATH_DB } from '../constants/contacts.js';
 
 export const writeContacts = async (updatedContacts) => {
   try {
-    let contact = JSON.stringify(updatedContacts); // з обєкту в стрічку
-    console.log('String', contact);
-    JSON.parse(contact); // з стрічки на обєкт
-    console.log('Parse', contact);
-    // await fs.appendFile(PATH_DB, array, 'utf-8');
+    await fs.appendFile(PATH_DB, updatedContacts, 'utf-8');
     console.log('File updated!');
   } catch (error) {
     console.log(error);
