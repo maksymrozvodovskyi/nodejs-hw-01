74476cfd9887e281db30bd37a87bf329b490f150
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-06-28 16:14:25 +0300

    add read and write

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    await fs.writeFile(PATH_DB, updatedContacts, 'utf-8');
+    console.log('File updated!');
+  } catch (error) {
+    console.log(error);
+  }
+};
