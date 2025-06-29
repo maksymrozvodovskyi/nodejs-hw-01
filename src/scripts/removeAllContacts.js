cddaa52fa7dd94b099285dc137b698cbfd345293
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-06-29 14:57:13 +0300

    end remove

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,13 @@
-export const removeAllContacts = async () => {};
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+
+export const removeAllContacts = async () => {
+  try {
+    const data = JSON.stringify([]);
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeAllContacts();
