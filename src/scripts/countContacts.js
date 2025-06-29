7dac81d67fb68e7f0fbdb0e1665468f4101d346f
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-06-29 14:43:16 +0300

    end count

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -2,8 +2,12 @@ import { readContacts } from '../utils/readContacts.js';
 
 export const countContacts = async () => {
   const contacts = await readContacts();
-  const count = contacts.length;
-  return count;
+  if (contacts > 0) {
+    const count = contacts.length;
+    return count;
+  } else {
+    console.log('No contacts yet!');
+  }
 };
 
 console.log(await countContacts());
